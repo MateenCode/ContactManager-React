9a5cb2d1d65121fa1215cec5c28da945d35e0cba
“mateen1222” <mateenkaziawebdev@gmail.com>
2018-09-05 17:57:42 -0700

    form state created

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import './App.css';
 
 import { Provider } from './context';
 
-import Contacts from './components/Contacts'
-import Header from './components/Header'
-
+import Contacts from './components/contacts/Contacts'
+import Header from './components/layout/Header'
+import AddContact from './components/contacts/AddContact'
 
 
 class App extends Component {
@@ -16,6 +16,7 @@ class App extends Component {
       <div className="App">
         <Header branding="Contact Manager" />
         <div className="container">
+        <AddContact />
         <Contacts />
         </div>
       </div>
